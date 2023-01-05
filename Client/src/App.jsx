41447684c853ef28c13c7424adcd27496494e373
Prathamesh-Chavan-232 <prathamesh.chavanpsc2018@gmail.com>
2023-01-05 07:18:33 +0530

    Cleaning up

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,7 +5,18 @@ import "./styles/App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<CheckAuth />} />
+          <Route exact path="/log-in" element={<Login />} />
+          <Route exact path="/sign-up" element={<Signup />} />
+          <Route exact path="/home" element={<Homepage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
