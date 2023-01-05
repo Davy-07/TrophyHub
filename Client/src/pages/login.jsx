fbd1ce683893fd8eb17724a948b53e09be3b0c7c
Prathamesh-Chavan-232 <prathamesh.chavanpsc2018@gmail.com>
2023-01-05 07:55:28 +0530

    Login page

--- Client/src/pages/login.jsx
+++ Client/src/pages/login.jsx
@@ -25,3 +25,59 @@ export const Login = () => {
     </div>
   );
 };
+// <div class="container">
+//   <h1 id="heading">Form validation using javascript</h1>
+//   <form novalidate action="" id="info">
+//     <h1>Registration</h1>
+//     <div class="fields">
+//       <label for="name">User-name</label>
+//       <input
+//         type="text"
+//         name="name"
+//         id="name"
+//         value=""
+//         placeholder="steve rogers"
+//       />
+//       <div class="error"></div>
+//     </div>
+//     <div class="fields">
+//       <label for="age">Age</label>
+//       <input type="text" name="age" id="age" value="" placeholder="112" />
+//       <div class="error"></div>
+//     </div>
+//     <div class="fields">
+//       <label for="email">Email</label>
+//       <input
+//         type="email"
+//         name="email"
+//         id="email"
+//         value=""
+//         placeholder="firstavenger@gmail.com"
+//       />
+//       <div class="error"></div>
+//     </div>
+//     <div class="fields">
+//       <label for="pass">Password</label>
+//       <input
+//         type="password"
+//         name="pass"
+//         id="pass"
+//         value=""
+//         placeholder="must have atleast 6 characters"
+//       />
+//       <div class="error"></div>
+//     </div>
+//     <div class="fields">
+//       <label for="confirm">Confirm Password</label>
+//       <input
+//         type="password"
+//         name="confirm"
+//         id="confirm"
+//         value=""
+//         placeholder="Retype password"
+//       />
+//       <div class="error"></div>
+//     </div>
+//     <button id="btn" type="submit">Submit</button>
+//   </form>
+// </div>
